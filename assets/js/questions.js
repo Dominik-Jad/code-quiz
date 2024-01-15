19383f9a0b7d80c96cda9f30f36d83a68e4334cc
Dominik Jadczak <123301714+Dominik-Jad@users.noreply.github.com>
2024-01-15 18:02:14 +0000

    added the quizQuestions object to the questions js file

---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+export const quizQuestions = [
+  {
+    question: "What is the purpose of the console.log() function in JavaScript?",
+    options: ["Input validation", "Data encryption", "Printing messages to the console", "Styling HTML elements"],
+    answer: 2,
+  },
+  {
+    question: "How do you declare a variable in JavaScript?",
+    options: ["new", "variable", "declare", "var, let, or const"],
+    answer: 3,
+  },
+  {
+    question: "What is the difference between let and const in variable declaration?",
+    options: ["let is for numbers, const is for strings", "let allows value changes, const is constant", "There is no difference", "let is for functions, const is for variables"],
+    answer: 1,
+  },
+  {
+    question: "How do you write a comment in JavaScript?",
+    options: ["comment:", `<-- -->`, "# This is a comment", "// or /* */"],
+    answer: 3,
+  },
+  {
+    question: "What is the purpose of the if statement in JavaScript?",
+    options: ["Defining a function", "Declaring variables", "Conditional execution", "Looping through arrays"],
+    answer: 2,
+  },
+  {
+    question: "What is an array in JavaScript?",
+    options: ["A string of characters", "A data structure that stores elements", "An HTML tag", "A type of loop"],
+    answer: 1,
+  },
+  {
+    question: "How do you loop through an array in JavaScript?",
+    options: ["loop(array)", "for-each(array)", "for (i = 0; i < array.length; i++)", "iterate(array)"],
+    answer: 2,
+  },
+  {
+    question: "What is the purpose of the function keyword in JavaScript?",
+    options: ["Defining a variable", "Declaring an array", "Creating a loop", "Declaring a function"],
+    answer: 3,
+  },
+  {
+    question: "How do you handle errors in JavaScript?",
+    options: ["catchError()", "try-except", "errorHandling()", "try-catch blocks"],
+    answer: 3,
+  },
+  {
+    question: "What is the DOM in the context of web development?",
+    options: ["Document Ordering Model", "Designated Object Model", "Document Object Model", "Dynamic Object Manipulation"],
+    answer: 2,
+  },
+];
