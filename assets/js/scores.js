deb298cf244d75c6e0984ae5cc9301d545baa829
Dominik Jadczak <123301714+Dominik-Jad@users.noreply.github.com>
2024-01-15 18:59:27 +0000

    added a function to display high scores in a list

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -3,6 +3,22 @@ var highScoresEl = document.querySelector("#highscores");
 var clearEl = document.querySelector("#clear");
 
 // function to display high scores
+function displayScores() {
+    var highScores = JSON.parse(localStorage.getItem("highScores"));
 
+    // if there are no high scores, display message
+    if (!highScores) {
+        highScoresEl.textContent = "No high scores to display.";
+        return;
+    }
+    for (var i = 0; i < highScores.length; i++) {
+        var score = document.createElement("li");
+        score.textContent = `${highScores[i].initials} - ${highScores[i].score}`;
+        highScoresEl.appendChild(score);
+    }
+}
+
+// when page is loaded display high scores
+displayScores();
 
 // function to clear high scores
